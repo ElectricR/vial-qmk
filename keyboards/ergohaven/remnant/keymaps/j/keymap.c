c67d92d5323104d8abef403fd78188c9873cea2f
ElectricR <chulkovroma@mail.ru>
2023-06-23 18:50:28 +0300

    polishing gaming layout

--- keyboards/ergohaven/remnant/keymaps/j/keymap.c
+++ keyboards/ergohaven/remnant/keymaps/j/keymap.c
@@ -23,34 +23,34 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DVORAK] = LAYOUT_5x6(
-     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_MINS,
+     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_LBRC,
+     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT,
+     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_BSLS,
                        KC_NO,   KC_NO,                                               KC_EQL,  KC_RBRC,
                                 L_BOTH,  KC_SPC,  KC_LSFT,         KC_RGUI, KC_ENT,  KC_RALT
   ),
   [_BOTH] = LAYOUT_5x6(
-     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,           KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
+     LT_GAM,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,           KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
      KC_NO,   KC_NO,   KC_NO,   KC_UP,   KC_NO,   KC_NO,           KC_NO,   M_LEFT,  KC_MS_U, M_RIGHT,  KC_NO,   KC_NO,
      RGB_MF,  KC_NO,   KC_LEFT, KC_DOWN, KC_RIGHT,KC_NO,           KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R,  KC_BSPC, KC_NO,
      RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, KC_NO,   KC_NO,           KC_NO,   KC_NO,   KC_WH_U, KC_NO,    KC_NO,   QK_BOOT,
                        RGB_VAD, RGB_VAI,                                             KC_WH_D, KC_NO,
-                                KC_NO,   KC_NO,   KC_NO,           KC_NO,   KC_NO,   LT_GAM
+                                KC_NO,   KC_NO,   KC_NO,           KC_NO,   KC_NO,   KC_NO
   ),
   [_GAMING1] = LAYOUT_5x6(
-     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,           KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,
-     KC_NO,   KC_NO,   KC_NO,   KC_W,    KC_NO,   KC_NO,           KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,
-     KC_NO,   KC_NO,   KC_A,    KC_S,    KC_D,    KC_NO,           KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,
-     KC_LCTL, KC_LALT, KC_NO,   KC_NO,   KC_NO,   KC_NO,           KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,
+     KC_GRV,  KC_NO,   KC_1,    KC_2,    KC_3,    KC_4,            KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
+     KC_ESC,  KC_T,    KC_Q,    KC_W,    KC_E,    KC_R,            KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
+     KC_TAB,  KC_G,    KC_A,    KC_S,    KC_D,    KC_F,            KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
+     KC_LCTL, KC_LALT, KC_Z,    KC_X,    KC_C,    KC_V,            KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
                        KC_NO,   KC_NO,                                               KC_NO,   KC_NO,
                                 L_GAM2,  KC_SPC,  KC_LSFT,         KC_NO,   KC_NO,   LT_GAM
   ),
   [_GAMING2] = LAYOUT_5x6(
-     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,           KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,
-     KC_NO,   KC_NO,   KC_NO,   KC_X,    KC_NO,   KC_NO,           KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,
-     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,           KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,
-     KC_LCTL, KC_LALT, KC_NO,   KC_NO,   KC_NO,   KC_NO,           KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,
+     KC_NO,   KC_NO,   KC_7,    KC_8,    KC_9,    KC_0,            KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
+     KC_NO,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,            KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
+     KC_NO,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,         KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
+     KC_LCTL, KC_LALT, KC_M,    KC_COMM, KC_DOT,  KC_SLSH,         KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
                        KC_NO,   KC_NO,                                               KC_NO,   KC_NO,
                                 KC_NO,   KC_NO,   KC_NO,           KC_NO,   KC_NO,   LT_GAM
   )

6b0b6b202cf7459a47c4f0d8da16de35fc996d2c
ElectricR <chulkovroma@mail.ru>
2024-03-15 17:05:53 +0300

    Changes

--- aoeu.sh
+++ aoeu.sh
@@ -2,6 +2,6 @@ sudo echo ok, now enter boot mode
 sleep 5
 mkdir -p mnt
 qmk compile -kb ergohaven/remnant -km j
-sudo mount /dev/sdd1 mnt/
+sudo mount /dev/sdc1 mnt/
 sudo cp ./ergohaven_remnant_j.uf2 mnt
-sudo umount /dev/sdd1
+sudo umount /dev/sdc1

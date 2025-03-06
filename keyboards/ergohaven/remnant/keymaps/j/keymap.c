bdeedf025a044c2a35085498cd568969d6f63487
ElectricR <roman.as.an.acquaintance@gmail.com>
2025-03-06 18:00:17 +0300

    .

--- keyboards/ergohaven/remnant/keymaps/j/keymap.c
+++ keyboards/ergohaven/remnant/keymaps/j/keymap.c
@@ -61,8 +61,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_GAMING1] = LAYOUT_5x6(
      KC_NO,   KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_P4,           KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
-     KC_ESC,  KC_T,    KC_Q,    KC_W,    KC_D,    KC_R,            KC_NO,   KC_MPLY, KC_MNXT, KC_MPRV,  KC_MSTP, KC_NO,
-     KC_TAB,  KC_F,    KC_A,    KC_S,    KC_E,    KC_G,            KC_NO,   KC_NO,   MYKEY,   KC_NO,    KC_NO,   KC_NO,
+     KC_ESC,  KC_T,    KC_Q,    KC_UP,   KC_D,    KC_R,            KC_NO,   KC_MPLY, KC_MNXT, KC_MPRV,  KC_MSTP, KC_NO,
+     KC_TAB,  KC_F,    KC_LEFT, KC_DOWN, KC_RIGHT,KC_G,            KC_NO,   KC_NO,   MYKEY,   KC_NO,    KC_NO,   KC_NO,
      KC_LCTL, KC_LALT, KC_X,    KC_C,    KC_V,    KC_B,            KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
                        KC_N,    KC_H,                                                KC_NO,   KC_NO,
                                 L_GAM2,  KC_SPC,  KC_LSFT,         KC_NO,   KC_NO,   KC_NO
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_NO,   KC_F14,  KC_P5,   KC_P6,   KC_P7,   KC_P8,           KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
      KC_NO,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,            KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
      KC_NO,   L_GAM3,  KC_J,    KC_K,    KC_L,    KC_SCLN,         KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
-     KC_LCTL, KC_LALT, KC_M,    KC_COMM, KC_DOT,  KC_SLSH,         KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
+     KC_LCTL, KC_LALT, KC_COMM, KC_M,    KC_DOT,  KC_SLSH,         KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
                        KC_P0,   KC_Z,                                                KC_NO,   KC_NO,
                                 KC_NO,   KC_NO,   KC_NO,           KC_NO,   KC_NO,   LT_GAM
   ),
